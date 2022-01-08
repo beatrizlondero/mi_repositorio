14c7ef87afd2d14edd130e028475e5a72db218b8
BeatrizLondero <beatriz04016@gmail.com>
2022-01-08 10:19:35 -0300

    mas avanzado sin probar

--- MVTBeatrizLondero/views.py
+++ MVTBeatrizLondero/views.py
@@ -1,3 +1,11 @@
 from django.http import HttpResponse
 from AppMVT.models import Parientes
+from django.template import Template, Context, loader
 
+def muestroParientes(request):
+    
+    template = loader.get_template('parientes.html')
+
+    documento = template.render(Parientes)
+
+    return HttpResponse(documento)
